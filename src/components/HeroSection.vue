b29aa0995df512ff123dd819812369a16eb8df3a
Natnael <natnael.tesfaye2022@gmail.com>
2025-07-03 21:16:05 +0300

    Nav and hero components

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,23 @@
+<script setup>
+    import {defineProps} from 'vue';
+
+    defineProps({
+        title:{
+            type: String,
+            default: "Become a Vue Dev"
+        },
+        subtitle:{
+            type: String,
+            default: "Find the Vue job that fits your skills and needs"
+        }
+    })
+</script>
+
+<template>
+    <section class="bg-green-700 py-20 mb-4 text-white">
+        <div class="flex flex-col items-center ">
+            <h1 class="text-4xl font-extrabold sm:text-5xl md:text-6xl font-sans">{{ title }}</h1>
+            <p class="text-xl my-4">{{ subtitle }}</p>
+        </div>
+    </section>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -14,6 +14,9 @@ export default defineConfig({
   ],
   server:{
     port: 3000,
+    hmr: {
+      overlay: false,
+    },
   },
   resolve: {
     alias: {
